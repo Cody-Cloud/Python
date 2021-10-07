21a26f8de58338f69bc8bba13e1dd971f4341cc0
Cody-Cloud <swindellscody@gmail.com>
2021-10-07 12:48:10 +1000

    Minor update and correction

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Main python projects and task.
 30/09/21 - Complete further tasks
 
 05/10/21 - Completed up to 5.3 day of tasks
+
+06/10/21 - Completed up to 5 Day Project
+
+Additional exit module for terminal based projects
---
+++ tasks/exit.py
@@ -0,0 +1,27 @@
+"""
+
+Author: Cody Swindells
+
+Created: 06/10/21
+
+Modfied: 06/10/21
+
+"""
+
+class exitProgram:
+    """
+
+    Main class to be called to further projects.
+
+    """
+    def main(program):
+        """
+        Main function that will use for further projects
+        for loops and exit for console based python projects.
+        """
+        exit = ""
+        while(exit != "exit"):
+            program
+            exit = input("Do you wish to exit program? \n").lower()
+            
+
