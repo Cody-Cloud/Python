5357c9afcc9a5b2706d548ae25299bcb6f3e6769
Cody-Cloud <swindellscody@gmail.com>
2021-12-13 12:42:13 +1000

    guess reveal function completed hangman project

--- tasks/hangman.py
+++ tasks/hangman.py
@@ -5,8 +5,9 @@ Author: Cody Swindells
 
 Created: 01/12/21
 
-Modified: 04/12/21
+Modified: 13/12/21
 
+Next stage: presenting hangman ascii art and progress til game is lost or won
 
 """
 import random
@@ -26,6 +27,12 @@ class hangman:
 
     user_select = ""
 
+    guess = ""
+
+    string_arr = []
+
+    letter_count = 0
+
     def select_word():
         """
         A function which selects a random word to be guess from a list.
@@ -38,7 +45,6 @@ class hangman:
         A function that present a certain number of blanks based on the word selected.
         
         """
-
         hangman.blank_word = '_'*len(hangman.chosen_word)
         print(hangman.blank_word)
 
@@ -47,27 +53,61 @@ class hangman:
         Checks a user letter guess against a randomly word from a list.
         
         """
-        guess = input(f"Please enter enter a letter. \n").lower()
+        hangman.guess = input(f"Please enter enter a letter. \n").lower()
 
-        if(guess in hangman.chosen_word):
+        if(hangman.guess in hangman.chosen_word):
             print("Letter is in.")
         else:
             print("Letter is not in.")
-
-
-
-
-    def main():
+    def remain_blank():
         """
-        Main function exection for hangman project.
-        
+        A function that contine 
+
+        """
+        if(hangman.guess == ""):
+            print("No guess made yet.")
+        else:
+            #hangman.string_arr.insert(hangman.chosen_word.index())
+
+            if(hangman.chosen_word.count(hangman.guess, 0 + hangman.letter_count, len(hangman.chosen_word) - 1) != 0):
+                #hangman.string_arr.insert(hangman.guess, hangman.chosen_word.find(hangman.guess) + 1)
+                hangman.string_arr[hangman.chosen_word.find(hangman.guess,  0 + hangman.letter_count, len(hangman.chosen_word) - 1)] = hangman.guess
+                hangman.letter_count += 1
+                hangman.remain_blank()
+            else:
+                print(''.join(hangman.string_arr)) #by using ''.join it simplys the conversion back to a string.
+            
+    def initalise_game ():
+        """
+        A function that control main initilsation of the game.
+
         """
         hangman.select_word()
 
         print(hangman.chosen_word)
 
         hangman.present_blank()
+
+        hangman.string_arr = list(hangman.blank_word)
+
+    def main():
+        """
+        Main function exection for hangman project.
+        
+        """
+
+
+        #will require a loop later with current computer selection and game instance
+
+        hangman.initalise_game()
         hangman.ask_user()
+        hangman.remain_blank()
+
+    def clear_game():
+        """
+        A function that reset all variables for reinitaiisation
+        
+        """
 
 
 
